55b319488f6798176227b2298553674b32291256
YUGESHKARAN <yugeshkaran01@gmail.com>
2025-04-12 20:26:47 +0530

    update-4: Navbar Updated

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -20,9 +20,11 @@ function NavBar() {
             
             <div className="xl:col-span-1 lg:col-span-2 col-span-1  flex justify-end ">
               {/* <button className="text-pink-500 font-semibold">Login</button> */}
-              <div onClick={()=>setshowMenu(true)} className="lg:hidden  flex items-center mr-2  justify-center ">
-              <MdOutlineMenu className="text-xl" />
+              <div onClick={()=>setshowMenu(!showMenu)} className="lg:hidden  flex items-center mr-2 transition-all-500 justify-center">
+              {showMenu?<IoClose className="text-2xl text-indigo-500" />:<MdOutlineMenu className="text-2xl text-indigo-500" />}  
               {/* menu */}
+
+             
             </div>
               <button className="bg-indigo-500 hidden md:block xl:text-base md:text-xs text-[10px] lg:text-sm text-white md:px-3 px-1 py-1 h-1/2 my-auto  rounded hover:bg-indigo-600 transition-all-800">
                 Get Started
@@ -31,17 +33,15 @@ function NavBar() {
           </header>
     
           {/* Side bar starts here */}
-          <div className={`${showMenu?'fixed right-0 w-[200px] z-50 h-full bg-indigo-200 ':'fixed right-[-400px] w-[200px] z-50 h-full bg-red-200 top-0'} top-0 transition-all-1000`}>
-            <ul className="flex-col items-center mt-10 space-y-10">
-              <li onClick={()=>{setshowMenu(false)}} className="w-11/12 flex justify-end items-center ">
-              <IoClose className="text-2xl"/>
-              {/* close */}
-              </li>
-              <li className="w-full text-center">menu1</li>
-              <li className="w-full text-center">menu1</li>
-              <li className="w-full text-center">menu1</li>
-              <li className="w-full text-center">menu1</li>
-              <li className="w-full text-center">menu1</li>
+          {/* <div className={`${showMenu?'fixed right-0 w-[200px] z-50 h-full bg-indigo-200 ':'fixed right-[-400px] w-[200px] z-50 h-full bg-red-200 top-0'} top-0 transition-all-1000`}> */}
+          <div className={`${showMenu?'relative w-full z-50 h-fit bg-white border-t ':'hidden'} transition-all-1000`}>
+            <ul className="absolute flex-col w-full z-50 h-fit bg-white border-t items-center text-sm font-semibold py-5  space-y-5">
+             
+              <li className="w-full text-center">Home</li>
+              <li className="w-full text-center">About</li>
+              <li className="w-full text-center">Our Services</li>
+              <li className="w-full text-center">Pricing</li>
+              <li className="w-full text-center">Contacts</li>
     
             </ul>
       
